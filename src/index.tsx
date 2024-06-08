1e088f2e8ee85256b08bde47c76019bda769e657
Tonny Piper <pipertonny@gmail.com>
2024-06-08 10:02:10 +0200

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class App extends React.Component {
+
+  render() {
+    return <div>Hello metsudo</div>
+  }
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
