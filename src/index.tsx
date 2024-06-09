5dbfc14fcbfd5116aa22dd6862df327c0886bdaa
Tonny Piper <pipertonny@gmail.com>
2024-06-09 13:35:34 +0200

    logic done, missing stilling

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-class App extends React.Component {
+const store = createStore(reducers, applyMiddleware(thunk));
 
-  render() {
-    return <div>Hello metsudo</div>
-  }
-}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
